34a070b96dd1e4089bf058dc05adf3e79b6e9776
EndriuK <endrit.kaskija@gmail.com>
2024-07-18 17:16:15 +0200

    [UPD] aggiungo "lista da fare" all'app

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 const { createApp } = Vue;
 
 createApp({
-  data() {},
+  data() {
+    return {
+      todos: [
+        { text: "Fare la spesa", done: false },
+        { text: "Portare il cane a spasso", done: true },
+        { text: "Andare a lavare la macchina", done: false },
+      ],
+    };
+  },
 }).mount("#app");
